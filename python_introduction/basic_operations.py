06e350136a42393f3b7413e2652cf313c3b579c0
KOBOKO23 <kobokomasero@gmail.com>
2025-09-12 01:39:00 +0300

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# This is a simple Python script that performs basic arithmetic operations.
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
+
